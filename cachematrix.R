30d0ae6ec9869498cd1de79801ea1406173d9539
wazhang <wangchun.zhang@ge.com>
2017-04-12 08:12:57 -0700

    implement the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions being implemented as follows:
+## 1 function "makeCacheMatrix" caching the mean of a matrix
+## 2:function "cacheSolve" returns a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
+
+## Cache the mean of a matrix so that for subsequence call of same matrix mean
+## there is no need to compute again, instead, retrieve from the cache.
+require(MASS)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse<-NULL
+  set<-function(y){
+     x<<-y
+     inverse<<-NULL
+  }
+  
+  get<-function() x
+  
+  setInverse <-function(inv) inverse<<-inv
+  getInverse <-function() inverse
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv<-x$getInverse()
+    if(!is.null(inv)) {
+       message("retrieve from cache")
+       inv
+    }
+    
+    data <-x$get()
+    inv <- ginv(data)
+    x$setInverse(inv)
+    inv
 }
